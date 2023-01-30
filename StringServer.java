c7ab4bb6dd372747d8ce60872008c134f87d5c3d
catllian <cllian@ucsd.edu>
2023-01-30 09:07:05 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.IOException;
+import java.net.URI;
+
+String[] stringList;
+
+class Handler {
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return null;
+        }
+        else if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            stringList.add(parameters[1]);
+            return null;
+        }
+        else if (url.getPath().contains("/search")) {
+            String[] returnList;
+            String[] parameters = url.getQuery().split("=");
+            for (int i = 0; i < stringList.length; i++) {
+                if (stringList[i].contains(parameters[1])) {
+                    returnList.add(stringList[i]);
+                }
+            }
+            return returnList;
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main (String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try an number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
