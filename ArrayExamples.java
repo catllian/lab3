4dfadbbf72458b15633dcd08a0139855e55543e6
catllian <cllian@ucsd.edu>
2023-02-13 03:19:00 -0800

    editted arrayexamples

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -28,22 +28,6 @@ public class ArrayExamples {
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
-  static double averageWithoutLowest(double[] arr) {
-    if(arr.length < 2) { return 0.0; }
-    double lowest = arr[0];
-    for(double num: arr) {
-      if(num < lowest) { lowest = num; }
-    }
-    double sum = 0;
-    for(double num: arr) {
-      if(num != lowest) { sum += num; }
-    }
-    return sum / (arr.length - 1);
-  }
-
-  // Averages the numbers in the array (takes the mean), but leaves out the
-  // lowest number when calculating. Returns 0 if there are no elements or just
-  // 1 element in the array
   //static double averageWithoutLowest(double[] arr) {
   //  if(arr.length < 2) { return 0.0; }
   //  double lowest = arr[0];
@@ -56,6 +40,23 @@ public class ArrayExamples {
   //  }
   //  return sum / (arr.length - 1);
   //}
+
+  // Averages the numbers in the array (takes the mean), but leaves out the
+  // lowest number when calculating. Returns 0 if there are no elements or just
+  // 1 element in the array
+  static double averageWithoutLowest(double[] arr) {
+    if(arr.length < 2) { return 0.0; }
+    double lowest = arr[0];
+    for(double num: arr) {
+      if(num < lowest) { lowest = num; }
+    }
+    double sum = 0;
+    int averageDivisor = 0;
+    for(double num: arr) {
+      if(num != lowest) { sum += num; averageDivisor += 1;}
+    }
+    return sum / (averageDivisor);
+  }
 
 
 }
