922dd7a50f00495770bb6212a927c8d2e5987c14
catllian <cllian@ucsd.edu>
2023-02-13 01:08:52 -0800

    Update StringServerTests.java

--- StringServerTests.java
+++ StringServerTests.java
@@ -5,14 +5,4 @@ import java.net.URI;
 
 public class StringServerTests {
 
-    @Test
-    public void test1TwoQueries() {
-    Handler test1Handler = new Handler();
-    URI test1arg1 = new URI("http://localhost:4000/add-message?s=test1FirstLine");
-    URI test1arg2 = new URI("http://localhost:4000/add-message?s=test1SecondLine");
-    testHandler1.handleRequest(test1arg1);
-    assertEquals("test1FirstLine" + "\n" + "test2SecondLine",
-            testHandler1.handleRequest(test1arg2));
-    }
-
 }
